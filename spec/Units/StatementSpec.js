dc7424c880999d0bc1586b5bde88de637aaf3192
CKKH <chriskkhassan@gmail.com>
2019-02-12 15:02:34 +0000

    Unit passing: Statement .calculateBalance records decrease for depoist withdrawals

--- spec/Units/StatementSpec.js
+++ spec/Units/StatementSpec.js
@@ -14,5 +14,13 @@ describe('Statement', () => {
       statement.calculateBalance()
       expect(statement.transactions[0][2]).toEqual(10)
     })
+
+    it('records decrease in running account balance for withdrawals', () => {
+      account = new Account()
+      account.withdraw(10)
+      statement = new Statement(account)
+      statement.calculateBalance()
+      expect(statement.transactions[0][2]).toEqual(-10)
+    })
   })
 })
