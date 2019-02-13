e74e42b7f2f38cdb73d97a7d5bd9fdaade1b5ab2
CKKH <chriskkhassan@gmail.com>
2019-02-13 18:41:12 +0000

    Unit: formatter formats transactions ready for print

--- spec/Units/FormatterSpec.js
+++ spec/Units/FormatterSpec.js
@@ -3,19 +3,13 @@
 'use strict'
 
 describe('Formatter', () => {
-  let account
-  let statement
-  let summary
-
-  describe('#format', () => {
-    it('formats the statement transactions', () => {
-      account = new Account()
-      account.deposit(10)
-      statement = new Statement(account)
-      statement.calculateBalance()
-      summary = new Summary(statement)
-      let formattedStatement = summary.format()
-      expect(formattedStatement[0]).toEqual(["12/02/2019 || 10.00 ||  || 10.00"])
+  describe('#process', () => {
+    it('formats deposit transactions for printing', () => {
+      let date = new Date().toLocaleDateString()
+      let transactions = [[date, 10, 10]]
+      let formatter = new Formatter()
+      let formattedStatement = formatter.process(transactions)
+      expect(formattedStatement[0]).toEqual(["13/02/2019 || 10.00 ||  || 10.00"])
     })
   })
 })
--- src/Formatter.js
+++ src/Formatter.js
@@ -1,29 +1,20 @@
 'use strict'
 
 function Formatter() {
-  this.formattedList = []
 }
 
-Formatter.prototype.format = function () {
-  let list = []
+Formatter.prototype.process = function (transactions) {
+  let formattedTransactions = []
   let date
   let credit
   let debit
   let balance
-
-  this.transactions.forEach(function(transaction) {
+  transactions.forEach(function(transaction) {
     date = transaction[0]
     credit = (transaction[1] > 0) ? transaction[1].toFixed(2) : ""
     debit = (transaction[1] < 0) ? -((transaction[1]).toFixed(2)) : ""
     balance = transaction[2].toFixed(2)
-    list.push([`${date} || ${credit} || ${debit} || ${balance}`])
+    formattedTransactions.push([`${date} || ${credit} || ${debit} || ${balance}`])
   })
-  return this.formattedList = list
-}
-
-Formatter.prototype.print = function() {
-  console.log("date || credit || debit || balance")
-  this.formattedList.slice().reverse().forEach(function(transaction) {
-    console.log(transaction)
-  })
+  return formattedTransactions
 }
