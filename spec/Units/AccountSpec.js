a8dd418b88dbbdc8982d0bb9939c6f9ed7606a3c
CKKH <chriskkhassan@gmail.com>
2019-02-13 15:15:50 +0000

    Unit test added: running balance saved in transactions

--- spec/Units/AccountSpec.js
+++ spec/Units/AccountSpec.js
@@ -4,21 +4,30 @@
 
 describe('Account', () => {
   let account
+  let balance
 
   beforeEach( () => {
-    account = new Account()
+    balance = new Balance()
+    account = new Account(balance)
   })
 
   describe('#deposit()', () => {
-    it('deposit value is recorded in account history', () => {
+    it('deposit value recorded in transactions', () => {
       account.deposit(50)
       expect(account.transactions[0]).toContain(50)
     })
 
-    it('deposit date is recorded in account history', () => {
+    it('deposit date is recorded in transactions', () => {
       let date = new Date().toLocaleDateString()
       account.deposit(50)
       expect(account.transactions[0]).toContain(date)
+    })
+
+    it('balance after deposit recorded in transactios', () => {
+      let date = new Date().toLocaleDateString()
+      account.deposit(50)
+      account.deposit(50)
+      expect(account.transactions[1]).toContain(100)
     })
   })
 
--- src/Balance.js
+++ src/Balance.js
@@ -4,7 +4,7 @@ function Balance() {
 }
 
 Balance.prototype.calculate = function (transactions) {
-  if (transactions.length == 1) {
+  if (transactions.length === 1) {
     transactions[0].push(transactions[0][1])
   } else if (transactions.length > 0) {
     let balance = (transactions[transactions.length -2][1]) + (transactions[transactions.length -1][1])
