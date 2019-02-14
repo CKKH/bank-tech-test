b10c16d6b25b2b5bd22fa7fb5277e4b408531705
CKKH <chriskkhassan@gmail.com>
2019-02-14 08:50:01 +0000

    Refactor/bug fix: debits formatted correctly, implemented Maths.abs()

--- src/Formatter.js
+++ src/Formatter.js
@@ -9,7 +9,7 @@ Formatter.prototype.process = function (transactions) {
   transactions.forEach(function(transaction) {
     let date = transaction[0]
     let credit = (transaction[1] > 0) ? transaction[1].toFixed(2) : ""
-    let debit = (transaction[1] < 0) ? -((transaction[1]).toFixed(2)) : ""
+    let debit = (transaction[1] < 0) ? Math.abs((transaction[1])).toFixed(2) : ""
     let balance = transaction[2].toFixed(2)
     formattedTransactions.push(`${date} || ${credit} || ${debit} || ${balance}`)
   })
