5bf704bf3f2a7ddd149a014f9c01dd5ee2887507
Chris Hassan <chriskkhassan@gmail.com>
2019-03-12 15:51:08 +0000

    Refactor: feature tests doesn't fail on different days

--- spec/Features/AccountFeatureSpec.js
+++ spec/Features/AccountFeatureSpec.js
@@ -2,12 +2,13 @@
 
 describe('Opening an account, depositing, withdrawing, and printing balance', () => {
   let account
+  let date = new Date().toLocaleDateString()
 
   it('Works', () => {
     account = new Account()
     account.deposit(1000)
     account.deposit(2000)
     account.withdraw(500)
-    expect(account.statement()).toEqual('date || credit || debit || balance\n16/02/2019 ||  || 500.00 || 2500.00\n16/02/2019 || 2000.00 ||  || 3000.00\n16/02/2019 || 1000.00 ||  || 1000.00')
+    expect(account.statement()).toEqual(`date || credit || debit || balance\n${date} ||  || 500.00 || 2500.00\n${date} || 2000.00 ||  || 3000.00\n${date} || 1000.00 ||  || 1000.00`)
   })
 })
